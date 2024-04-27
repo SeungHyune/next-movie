4947be1ae5dcbd5c80fd1784a6afee240da9e93b
seunghyun <tmdgus7820@naver.com>
2024-04-27 14:18:24 +0900

    [✨feat] : App Router 구조 설정 - 페이지 단위로 분리 (홈, 검색 페이지, 영화 상세 정보 페이지)

---
+++ src/app/movie/info/page.tsx
@@ -0,0 +1,9 @@
+const MovieInfoPage = () => {
+  return (
+    <section>
+      <h1>영화 정보 페이지입니다.</h1>
+    </section>
+  )
+}
+
+export default MovieInfoPage
---
+++ src/app/movie/page.tsx
@@ -0,0 +1,9 @@
+const MoviePage = () => {
+  return (
+    <section>
+      <h1>영화 검색 결과 페이지입니다.</h1>
+    </section>
+  )
+}
+
+export default MoviePage
